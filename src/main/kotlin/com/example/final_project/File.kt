44c637e6de1cd2e51a922e01a22a43e368940819
michalaforest3 <200252967@student.georgianc.on.ca>
2024-11-05 22:00:20 -0500

    Added text container and add new button to the IDE

---
+++ src/main/kotlin/com/example/final_project/File.kt
@@ -0,0 +1,8 @@
+package com.example.final_project
+
+class File {
+
+    private var fileName: String = "Untitled"
+    private var fileContent: String = "This is a new file! Start writing code here!"
+
+}
--- src/main/kotlin/com/example/final_project/IDEController.kt
+++ src/main/kotlin/com/example/final_project/IDEController.kt
@@ -1,4 +1,27 @@
 package com.example.final_project
 
-class IDEController {
+import javafx.event.ActionEvent
+import javafx.fxml.FXML
+import javafx.fxml.Initializable
+import javafx.scene.control.Button
+import javafx.scene.control.TextArea
+import java.net.URL
+import java.util.*
+
+class IDEController : Initializable {
+    @FXML
+    private lateinit var textContent: TextArea
+
+    @FXML
+    private val newFile: Button? = null
+
+    override fun initialize(url: URL?, resourceBundle: ResourceBundle?) {
+
+        textContent.text = "Hello World"
+    }
+
+    @FXML
+    fun createNewFile(event: ActionEvent?) {
+        println("button clicked!")
+    }
 }
