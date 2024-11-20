e31748ad73d7bf1a3f55a436580206ad81fc5eef
michalaforest3 <200252967@student.georgianc.on.ca>
2024-11-19 19:41:57 -0500

    Changed file structure to be able to create .jar file

--- src/main/kotlin/com/example/final_project/App.kt
+++ src/main/kotlin/com/example/final_project/App.kt
@@ -5,16 +5,23 @@ import javafx.fxml.FXMLLoader
 import javafx.scene.Scene
 import javafx.stage.Stage
 
-class Main : Application() {
+class App : Application() {
     override fun start(stage: Stage) {
-        val fxmlLoader = FXMLLoader(Main::class.java.getResource("ide.fxml"))
+        val fxmlLoader = FXMLLoader(App::class.java.getResource("ide.fxml"))
         val scene = Scene(fxmlLoader.load())
         stage.title = "KOTLIN IDE"
         stage.scene = scene
         stage.show()
     }
+
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+            Application.launch(App::class.java)
+        }
+    }
 }
 
-fun main() {
-    Application.launch(Main::class.java)
-}
+//fun main() {
+//    Application.launch(App::class.java)
+//}
